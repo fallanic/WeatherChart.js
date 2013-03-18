2ebeb954338207377a662fd9892f0cb748d6e55b
Fabien Allanic <fabien@foodtree.com>
2013-03-18 16:18:25 -0700

    implementing the charting with only one value

--- WeatherChart.js
+++ WeatherChart.js
@@ -30,9 +30,15 @@ function roundRect(ctx,x, y, width, height, radius, fill, stroke) {
 function getMinAndMax(data){
 	var minMin=null;
 	var maxMax=null;
-	for(var i=0;i<data.length;i++){
+	for(var i=0;i<data.length;i++){		
 		var min = data[i][1];
-		var max = data[i][2];
+		//if only one value
+		var max = data[i][1];
+		if(data[i].length==3){
+			//if two values
+			max = data[i][2];
+		}
+		
 		if(minMin==null||min<minMin){
 			minMin = min;
 		}
@@ -46,89 +52,133 @@ function getMinAndMax(data){
 
 function weatherChart(options){
 	if(!options.data){alert("No data");}else{
-		var canvas_width = options.canvas_width;
-		var canvas_height = options.canvas_height;
-		if(!canvas_width){canvas_width = $("#"+options.canvas_id).parent().width();}
-		if(!canvas_height){canvas_height = $(window).height()-30;}
-		if(!options.colors){options.colors = ["#59B6DF","#FEB431"];}
-		if(!options.font){options.font="16px sans-serif";}
-		if(!options.proportions){options.proportions = [20,65,15]}
 
-		//we need a minumum size for the chart
-		if(canvas_height < 320) canvas_height = 320;
+		//for each row we have one date and one or two values
+		var dimensions = options.data[0].length - 1;
 
-		// Now you can just call
-		var ctx = document.getElementById(options.canvas_id).getContext("2d");
+		if(dimensions < 1 || dimensions > 2){
+			alert('Wrong data');
+		}else{
 
-		ctx.canvas.width = canvas_width;//don't know where this bloody offset comes from
-		ctx.canvas.height = canvas_height;
+			var canvas_width = options.canvas_width;
+			var canvas_height = options.canvas_height;
+			if(!canvas_width){canvas_width = $("#"+options.canvas_id).parent().width();}
+			if(!canvas_height){canvas_height = $(window).height()-30;}
+			if(!options.colors){options.colors = ["#59B6DF","#FEB431"];}
+			if(!options.font){options.font="16px sans-serif";}
+			if(!options.proportions){
+				if(dimensions==1){
+					options.proportions = [20,80];
+				}else{
+					options.proportions = [20,65,15];
+				}				
+			}
+			if(!options.unit){options.unit = ""}
 
-		//WIDTH : by default a bar is 20% a date, 65% a barchart (with min temp inside), 15% a max temp
-		var dateColumnWidth = Math.floor((options.proportions[0]/100)*canvas_width);
-		var barChartColumnWidth = Math.floor((options.proportions[1]/100)*canvas_width);
-		var maxTempColumnWidth = Math.floor((options.proportions[2]/100)*canvas_width);
+			//we need a minumum size for the chart
+			if(canvas_height < 320) canvas_height = 320;
 
-		//HEIGHT : with a week data, a bar height is 1/7 of the canvas
-		var barRowHeight = Math.floor(canvas_height/options.data.length);
+			// getting the canvas context
+			var ctx = document.getElementById(options.canvas_id).getContext("2d");
 
-		//the smallest value will take one quarter of the barChartColumnWidth
-		var minMinWidth = barChartColumnWidth/4;	
-		// the biggest value will take all of barChartColumnWidth 
-		var maxMaxWidth = barChartColumnWidth;
+			ctx.canvas.width = canvas_width;//don't know where this bloody offset comes from
+			ctx.canvas.height = canvas_height;
 
-		var minAndMax = getMinAndMax(options.data);
-		var minMin = minAndMax[0];
-		var maxMax = minAndMax[1];
+			//WIDTH : by default a bar is 20% a date, 65% a barchart (with min value inside), 15% a max value (20/80 if no max value)
+			var dateColumnWidth = Math.floor((options.proportions[0]/100)*canvas_width);
+			var barChartColumnWidth = Math.floor((options.proportions[1]/100)*canvas_width);
+			/*if(dimensions == 2){
+				var maxValueColumnWidth = Math.floor((options.proportions[2]/100)*canvas_width);
+			}*/
 
-		//this is the coeefficient needed to calculate each width
-		var one_degree_width = (maxMaxWidth - minMinWidth)/(maxMax - minMin);
+			//HEIGHT : with a week data, a bar height is 1/7 of the canvas
+			var barRowHeight = Math.floor(canvas_height/options.data.length);
 
-		for(var i=0;i<options.data.length;i++){
-			row_data = options.data[i];
-			
-			//calculating several values
-			var xOffset = 0;
-			var paddingTopAndBottom = 10;
-			yOffset = paddingTopAndBottom+i*barRowHeight;
+			//the smallest value will take one quarter of the barChartColumnWidth
+			var minMinWidth = barChartColumnWidth/4;	
+			// the biggest value will take all of barChartColumnWidth 
+			var maxMaxWidth = barChartColumnWidth;
 
-			//small bar = minimum width + the additional degrees * coefficient
-			var smallBarWidth = minMinWidth + (row_data[1] - minMin)*one_degree_width;
-			//long bar = maximum width - the number of degrees needed to reach the maximum value
-			var longBarWidth = maxMaxWidth - (maxMax - row_data[2])*one_degree_width;
-			//removing the paddings on top and bottom
-			var bothBarHeight = barRowHeight-paddingTopAndBottom*2; 
+			var minAndMax = getMinAndMax(options.data);
+			var minMin = minAndMax[0];
+			var maxMax = minAndMax[1];
 
-			// 1) drawing date
-			ctx.fillStyle = "#000";
-			ctx.font = options.font;
-			ctx.fillText(row_data[0], xOffset, yOffset+bothBarHeight*2/3);
+			//this is the coeefficient needed to calculate each width
+			var one_degree_width = (maxMaxWidth - minMinWidth)/(maxMax - minMin);
 
-			// 2) drawing bars		
-			xOffset = dateColumnWidth;
+			for(var i=0;i<options.data.length;i++){
+				row_data = options.data[i];
+				
+				//calculating several values
+				var xOffset = 0;
+				var paddingTopAndBottom = 10;
+				yOffset = paddingTopAndBottom+i*barRowHeight;
 
-			//drawing the larger bar
-			ctx.strokeStyle = options.colors[1];
-			ctx.fillStyle = options.colors[1];
-			roundRect(ctx,xOffset, yOffset, longBarWidth, bothBarHeight, 10, true);
+				if(dimensions == 2){
+					//small bar = minimum width + the additional degrees * coefficient
+					var smallBarWidth = minMinWidth + (row_data[1] - minMin)*one_degree_width;
+					
+					//long bar = maximum width - the number of degrees needed to reach the maximum value
+					var longBarWidth = maxMaxWidth - (maxMax - row_data[2])*one_degree_width;
 
-			//drawing the smaller bar on top
-			ctx.strokeStyle = options.colors[0];
-			ctx.fillStyle = options.colors[0];
-			roundRect(ctx,xOffset, yOffset, smallBarWidth, bothBarHeight, 10, true);
+					//removing the paddings on top and bottom
+					var bothBarHeight = barRowHeight-paddingTopAndBottom*2; 
 
-			//drawing the temperature
-			ctx.fillStyle = "#FFFFFF";
-			ctx.fillText(row_data[1]+"˚", xOffset+20, yOffset+bothBarHeight*2/3);
+					// 1) drawing date
+					ctx.fillStyle = "#000";
+					ctx.font = options.font;
+					ctx.fillText(row_data[0], xOffset, yOffset+bothBarHeight*2/3);
 
-			// 3) drawing max temp
-			xOffset = dateColumnWidth+barChartColumnWidth;
-			ctx.fillStyle = "#000";
-			ctx.fillText(row_data[2]+"˚", xOffset+10, yOffset+bothBarHeight*2/3);
-		}
+					// 2) drawing bars		
+					xOffset = dateColumnWidth;
 
-		if(options.redraw && options.redraw === true){
-			options.redraw = false;
-			window.addEventListener('resize', function(){weatherChart(options);}, false);				
+					//drawing the larger bar
+					ctx.strokeStyle = options.colors[1];
+					ctx.fillStyle = options.colors[1];
+					roundRect(ctx,xOffset, yOffset, longBarWidth, bothBarHeight, 10, true);
+
+					//drawing the smaller bar on top
+					ctx.strokeStyle = options.colors[0];
+					ctx.fillStyle = options.colors[0];
+					roundRect(ctx,xOffset, yOffset, smallBarWidth, bothBarHeight, 10, true);
+
+					//drawing the temperature
+					ctx.fillStyle = "#FFFFFF";
+					ctx.fillText(row_data[1]+options.unit, xOffset+20, yOffset+bothBarHeight*2/3);
+
+					// 3) drawing max value
+					xOffset = dateColumnWidth+barChartColumnWidth;
+					ctx.fillStyle = "#000";
+					ctx.fillText(row_data[2]+options.unit, xOffset+10, yOffset+bothBarHeight*2/3);
+				}else{
+					longBarWidth = maxMaxWidth - (maxMax - row_data[1])*one_degree_width;
+
+					//removing the paddings on top and bottom
+					var bothBarHeight = barRowHeight-paddingTopAndBottom*2; 
+
+					// 1) drawing date
+					ctx.fillStyle = "#000";
+					ctx.font = options.font;
+					ctx.fillText(row_data[0], xOffset, yOffset+bothBarHeight*2/3);
+
+					// 2) drawing bars		
+					xOffset = dateColumnWidth;
+
+					//drawing the larger bar
+					ctx.strokeStyle = options.colors[0];
+					ctx.fillStyle = options.colors[0];
+					roundRect(ctx,xOffset, yOffset, longBarWidth, bothBarHeight, 10, true);
+
+					//drawing the temperature
+					ctx.fillStyle = "#FFFFFF";
+					ctx.fillText(row_data[1]+options.unit, xOffset+20, yOffset+bothBarHeight*2/3);
+				}
+			}
+
+			if(options.redraw && options.redraw === true){
+				options.redraw = false;
+				window.addEventListener('resize', function(){weatherChart(options);}, false);				
+			}
 		}
 	}
 }
